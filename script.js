f3d7233d3c79b7d9f00757ebfa35e1b9ad55eb50
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-02-08 15:50:32 +0100

    Commit du 08.02.23 (après-midi)

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ city_name.innerHTML = "Nancy";
     }
 
     button.addEventListener("click", function(){
+        const result_array = [];
 
         navigator.geolocation.getCurrentPosition((position) => {
             let lat = position.coords.latitude;
@@ -47,22 +48,67 @@ city_name.innerHTML = "Nancy";
                 .then(response => response.json())
                 .then((data) => {
                     console.log(data);
-                })
-                    
+                    getNearestDate(getCurrentTimestamp());
 
-            
-            
-        }) 
-         
+                    function getCurrentTimestamp(){
+                        return Date.now();
+                    }
+                    
+                    function getNearestDate(currentDate){
+                        for(let i = 0; i <= data.hourly.time.length - 1 ; i++){
+                            let result = Math.sqrt((data.hourly.time[i] - currentDate)**2 + (data.hourly.time[i] - currentDate)**2);
+                            console.log(result);
+                            result_array.push(result);
+                            let lowest_value = Math.min.apply(Math, result_array);
+                            var lowest_value_key = result_array.indexOf(lowest_value); 
+                            console.log(lowest_value_key);
+                            
+                            
+                                                                          
+                        }
+
+
+                    }
+                })        
+        })      
     })
           
         function APIurl(lat, lon){
             console.log(lat);
             console.log(lon);
-            return url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&hourly=temperature_2m,weathercode"
+            return url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + "&hourly=temperature_2m,weathercode&timeformat=unixtime"
         }
 
+        function convertStampDate(unixtimestamp){
+                
+            var months_arr = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
+            // Convert timestamp to milliseconds:
+            var date = new Date(unixtimestamp);
+            // Year:
+            var year = date.getFullYear();
+            // Month:
+            var month = months_arr[date.getMonth()];
+            // Day:
+            var day = date.getDate();
+            // Hours:
+            var hours = date.getHours();
+            // Minutes:
+            var minutes = "0" + date.getMinutes();
+            // Seconds:
+            var seconds = "0" + date.getSeconds();
+            // String for displaying date and time in dd-MM-yyyy h:m:s format:
+            var fulldate = day+' '+month+' '+year+' - '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            
+            return fulldate;
+            }
 
+
+
+
+           
+
+
+            
 
 
         
